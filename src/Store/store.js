9e8fb8796198a0da657a92ebb74f5cf8a13d1230
MahmoudAtiaEed <mahmod02015@gmial.com>
2023-04-27 22:10:55 +0300

    first commit

---
+++ src/Store/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from '../features/Counter/counterSlice'
+
+export default configureStore({
+  reducer: {
+    counter: counterReducer
+
+  }
+})
+
+
